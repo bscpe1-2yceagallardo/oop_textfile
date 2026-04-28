75b58b2762f9eadf9280cfa78157d24791cc41c8
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-04-29 00:00:32 +0800

    Added the computing_statistics in the processor

--- numbers_separation/even_odd_processor.py
+++ numbers_separation/even_odd_processor.py
@@ -3,3 +3,4 @@ from even_and_odd_separation import NumberDistinguisher
 processor = NumberDistinguisher("numbers.txt")
 processor.scan_and_distinguish()
 processor.saving_results()
+processor.computing_statistics()
