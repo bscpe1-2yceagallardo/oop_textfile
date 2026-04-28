a05fd51e700a199ad194ad8583e74d0fb20acc41
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-04-28 23:32:37 +0800

    Added the data categorizing to even, odd, or invalid

--- numbers_separation/even_and_odd_separation.py
+++ numbers_separation/even_and_odd_separation.py
@@ -17,3 +17,16 @@ class NumberDistinguisher:
                         self.odd_numbers_collection.append(numeric_entry)
                 except ValueError:
                     self.invalid_entries.append(cleaned_entry)
+
+    def saving_results(self, even_file="even.txt", odd_file="odd.txt", invalid_file="invalid.txt"):
+        with open(even_file, 'w') as evens_file:
+            for even_value in self.even_numbers_collection:
+                evens_file.write(str(even_value) + "\n")
+
+        with open(odd_file, 'w') as odds_file:
+            for odd_value in self.odd_numbers_collection:
+                odds_file.write(str(odd_value) + "\n")
+
+        with open(invalid_file, 'w') as invalids_file:
+            for bad_value in self.invalid_entries:
+                invalids_file.write(str(bad_value) + "\n")
