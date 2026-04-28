b3d13cacc93c0835a56156c9b09e3056a93962d5
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-04-28 23:17:28 +0800

    Uploaded the data validation and classifying tool

--- numbers_separation/even_and_odd_separation.py
+++ numbers_separation/even_and_odd_separation.py
@@ -4,3 +4,16 @@ class NumberDistinguisher:
         self.even_numbers_collection = []
         self.odd_numbers_collection = []
         self.invalid_entries = [] # For the non-integer values
+
+    def scan_and_distinguish(self):
+        with open(self.numbers, 'r') as number_file:
+            for line_entry in number_file:
+                cleaned_entry = line_entry.strip()
+                try:
+                    numeric_entry = int(cleaned_entry)
+                    if numeric_entry % 2 == 0:
+                        self.even_numbers_collection.append(numeric_entry)
+                    else:
+                        self.odd_numbers_collection.append(numeric_entry)
+                except ValueError:
+                    self.invalid_entries.append(cleaned_entry)
