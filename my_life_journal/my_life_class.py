bd06efb76ac26306a2a09ec836b446ac47c06e3a
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-04-29 04:09:02 +0800

    Uploaded the class and constructor with bible verse library

---
+++ my_life_journal/my_life_class.py
@@ -0,0 +1,22 @@
+class MyLifeArchives:
+    def __init__(self, archive_name="mylife.txt"):
+        self.archive_name = archive_name
+        self.lines_commited = 0
+        # Verse Library for God's Affirmation
+        self.bible_verses = {
+            "happy": [
+                "Psalms 118:24 - 'This is the day the Lord has made; let us rejoice and be glad in it.'",
+                "Proverbs 17:22 - 'A chherful heart is good medicine.'",
+                "Philippians 4:4 - 'Rejoice in the Lord always. I will say it again: Rejoice!'"
+            ],
+            "sad": [
+                "Psalms 34:18 - 'The Lord is close to the brokenhearted and saves those who are crushed in spirit.'",
+                "Matthew 5:4 - 'Blessed are those who mourn, for they will be comforted.'",
+                "Joshua 1:9 - 'Do not be afraid; do not be discourage, for the Lord your God will be with you wherever you go.'"
+            ],
+            "general": [
+                "Philippians 4:13 - 'I can do all things through Christ who strengthens me.'",
+                "Jeremiah 29:11 - 'For I know the plans I have for you, declares the Lord.'"
+            ]
+        }
+        
