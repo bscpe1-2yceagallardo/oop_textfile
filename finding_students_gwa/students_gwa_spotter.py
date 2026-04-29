4dc9f157950cb44a1a8fd214608dcc0fcd4e0f42
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-04-29 13:15:41 +0800

    Added honor category logic

--- finding_students_gwa/students_gwa_spotter.py
+++ finding_students_gwa/students_gwa_spotter.py
@@ -12,3 +12,13 @@ class StudentGwaSpotter:
                         self.students_records.append((full_name.strip(), float(gwa_value)))
         except FileNotFoundError:
             print("Error:  The data file was not found.")
+
+    def get_honor_student(self, gwa):
+        if 1.0 <= gwa <= 1.25:
+            return "President's Lister 🏆"
+        elif 1.26 <= gwa <= 1.50:
+            return "Dean's Lister ⭐"
+        elif 1.51 <= gwa <= 1.75:
+            return "Honor Roll 🎖️"
+        else:
+            return "Passing"
