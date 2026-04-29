b0cacfe8c34d193cfdaba7c90e9f7b96a5c4510d
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-04-29 14:01:51 +0800

    Added the logic of selection

--- finding_students_gwa/students_gwa_analyzer.py
+++ finding_students_gwa/students_gwa_analyzer.py
@@ -14,3 +14,40 @@ def main():
         print("5. Exit")
 
         user_choice = input("\nSelect an option: ")
+
+        if user_choice == "1":
+            name, gwa, status = finder.finding_highest_gwa()
+            print(f"\nTOP STUDENT:\n{name} - {gwa:.2f} ({status})")
+
+        elif user_choice == "2":
+            top_performers = finder.top_three()
+            print("\n--- TOP 3 ACADEMIC ACHIEVERS---")
+            for rank, (name, gwa, status) in enumerate(top_performers, start=1):
+                print(f"{rank}. {name} | GWA: {gwa:.2f} | {status}")
+
+        elif user_choice == "3":
+            avg, high, low = finder.get_class_analytics()
+            print("\n--- CLASS ANALYTICS ---")
+            print(f"Class Average: {avg}")
+            print(f"Best Grade: {high}")
+            print(f"Lowest Grade: {low}")
+
+        elif user_choice == "4":
+            search_query = input("Enter student name: ")
+            result = finder.searching_student(search_query)
+            if result:
+                print(f"\n Match Found: {result['name']}")
+                print(f"Rank: #{result['rank']} | GWA: {result['gwa']} | {result['status']}")
+            else:
+                print("\nNo student found with that name.")
+
+        elif user_choice == "5":
+            print("\nShutting down system. Goodbye!")
+            break
+        else:
+            print("Invalid selection. Please try again.")
+
+if __name__ == "__main__":
+    main()
+
+
