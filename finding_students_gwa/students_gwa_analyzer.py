80f60027d0c471dd4f7e143b7a217a40529a639c
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-04-29 13:44:12 +0800

    Added Academic Record System dashboard

--- finding_students_gwa/students_gwa_analyzer.py
+++ finding_students_gwa/students_gwa_analyzer.py
@@ -3,3 +3,14 @@ from students_gwa_spotter import StudentGwaSpotter
 def main():
     finder= StudentGwaSpotter("students_gwa.txt")
     finder.load_students()
+
+    while True:
+        print("\n" + "=" * 30)
+        print("ACADEMIC RECORD SYSTEM")
+        print("1. Show the Top Student")
+        print("2. Show the Top 3 Performers")
+        print("3. View Class Analytics")
+        print("4. Search specific student by name")
+        print("5. Exit")
+
+        user_choice = input("\nSelect an option: ")
