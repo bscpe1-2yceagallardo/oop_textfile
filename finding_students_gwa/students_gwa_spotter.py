a92c54e4bdd9dc64ffec38c4b9015f793a43d092
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-04-29 13:11:14 +0800

    Added reading and parsing logic and error handling

--- finding_students_gwa/students_gwa_spotter.py
+++ finding_students_gwa/students_gwa_spotter.py
@@ -2,3 +2,13 @@ class StudentGwaSpotter:
     def __init__(self, students_gwa):
         self.students_gwa = students_gwa
         self.students_records = []
+
+    def load_students(self):
+        try:
+            with open(self.students_gwa, 'r') as file_link:
+                for line in file_link:
+                    if "," in line:
+                        full_name, gwa_value = line.strip().split(",")
+                        self.students_records.append((full_name.strip(), float(gwa_value)))
+        except FileNotFoundError:
+            print("Error:  The data file was not found.")
